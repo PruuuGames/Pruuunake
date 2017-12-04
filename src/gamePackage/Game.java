8e0f551a7ed44c304c6778c9086c6886bddb3143
Rodrigo Cunha <rcs8@cin.ufpe.br>
2017-12-03 23:15:22 -0300

    Signed Off By Rodrigo Cunha

---
+++ src/gamePackage/Game.java
@@ -0,0 +1,33 @@
+package gamePackage;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+
+public class Game extends javax.swing.JFrame {
+
+	public Game() {
+		Initialize();
+
+	}
+
+	public void Initialize() {
+		JPanel mainPanel = new JPanel();
+		getContentPane().add(mainPanel, BorderLayout.CENTER);
+
+		javax.swing.GroupLayout mainPanelLayout = new javax.swing.GroupLayout(mainPanel);
+		mainPanel.setLayout(mainPanelLayout);
+
+		mainPanelLayout.setHorizontalGroup(mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGap(0, 132, Short.MAX_VALUE));
+
+		mainPanelLayout.setVerticalGroup(mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGap(0, 88, Short.MAX_VALUE));
+
+		javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+		getContentPane().setLayout(layout);
+		layout.setHorizontalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(layout.createSequentialGroup().addContainerGap()
+				.addComponent(mainPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE).addContainerGap()));
+
+		layout.setVerticalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(layout.createSequentialGroup().addContainerGap()
+				.addComponent(mainPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE).addContainerGap()));
+	}
+}
