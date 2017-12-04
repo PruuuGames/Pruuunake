b3e87ca5c7dbb899d0af19ac2046881af23a7a2f
Rodrigo Cunha <rcs8@cin.ufpe.br>
2017-12-04 19:13:56 -0300

    Signed-off-by: Rodrigo Cunha <rcs8@cin.ufpe.br>

--- src/br/ufpe/cin/plc/views/Field.java
+++ src/br/ufpe/cin/plc/views/Field.java
@@ -7,55 +7,55 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
-public class Field extends JPanel{
+public class Field extends JPanel {
 
-	private static final int WIDTH = 800/20;
-	private static final int HEIGHT = 800/20;
-	
+	private static final int BUTTON_WIDTH = 40;
+	private static final int BUTTON_HEIGHT = 40;
+
 	private int rows;
 	private int columns;
 
 	private JButton[][] field;
 
 	public Field(char[][] field) {
-			this.rows = field.length;
-			this.columns = field[0].length;
-			
-			this.field = new JButton[rows][columns];
-			
-			initializeField(field);
-			buildField(field);
-	}	
-	
+		this.rows = field.length;
+		this.columns = field[0].length;
+
+		this.field = new JButton[rows][columns];
+
+		initializeField(field);
+		buildField(field);
+	}
+
 	public void initializeField(char[][] field) {
 		for (int row = 0; row < rows; row++) {
 			for (int column = 0; column < columns; column++) {
 				this.field[row][column] = new JButton();
-				this.field[row][column].setBounds(row * WIDTH, column * HEIGHT, WIDTH, HEIGHT);
+				this.field[row][column].setBounds(row * BUTTON_WIDTH, column * BUTTON_HEIGHT, BUTTON_WIDTH, BUTTON_HEIGHT);
 				add(this.field[row][column]);
 			}
 		}
 	}
-	
+
 	public void buildField(char[][] field) {
 		for (int row = 0; row < rows; row++) {
-			for (int column = 0; column < columns; column++) {				
+			for (int column = 0; column < columns; column++) {
 				if (field[row][column] == ' ') {
 					this.field[row][column].setBackground(Color.WHITE);
-					
-				}else if (field[row][column] == 'A') {
+
+				} else if (field[row][column] == 'A') {
 					this.field[row][column].setBackground(Color.BLUE);
-					
-				}else if (field[row][column] == 'B') {
+
+				} else if (field[row][column] == 'B') {
 					this.field[row][column].setBackground(Color.RED);
-					
-				}else if (field[row][column] == 'a') {
+
+				} else if (field[row][column] == 'a') {
 					this.field[row][column].setIcon(new ImageIcon("src/br/ufpe/cin/pombo.jpg"));
-					
-				}else if (field[row][column] == 'b') {
+
+				} else if (field[row][column] == 'b') {
 					this.field[row][column].setIcon(new ImageIcon("src/br/ufpe/cin/pombo.jpg"));
-					
-				}else if (field[row][column] == 'X') {
+
+				} else if (field[row][column] == 'X') {
 					this.field[row][column].setIcon(new ImageIcon("src/br/ufpe/cin/pizza.png"));
 				}
 			}
